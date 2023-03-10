eacc0ff987f84ecd7e77eb38dac363509a8ca280
Zikam <isaacaku1@gmail.com>
2023-03-10 08:32:19 +0100

    Page-Reseize

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
     <div id="contact"
       class="w-full bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
-      <div class="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+      <div class="flex flex-col lg:h-screen p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div class="pb-8">
           <p class="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -111,7 +111,7 @@ export default defineComponent({
 <div id="experience"
       className="bg-gradient-to-b from-gray-800 to-black w-full "
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white lg:h-screen">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
